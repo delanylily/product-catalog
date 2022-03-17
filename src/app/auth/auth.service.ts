b0c47890f6692d4d5226374765c18dfedecca9a9
lily <lceciledesign@gmail.com>
2022-03-17 10:14:44 +0100

    auth user * error handling in auth service added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { User } from './auth/user.model';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) { }
 
@@ -25,16 +28,53 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(errorResponse => {
-      let errorMessage = 'An unknown error occurred!';
-      if (!errorResponse.error || !errorResponse.error.error) {
-        return throwError(errorMessage);
+    ).pipe(catchError(this.handleError),
+      tap(response => {
+        this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn)
+      }));
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate);
+    this.user.next(user);
+  }
+
+  logIn(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCHJpMLj6_vpbNMqIMiGFZbrboBDA9YPN0',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
       }
-      switch (errorResponse.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email already exists';
-      }
-      return throwError(errorMessage)
+    ).pipe(catchError(this.handleError), tap(response => {
+      this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn)
     }));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+    return throwError(errorMessage)
+
   }
 }
--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -26,19 +27,21 @@ export class AuthComponent implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>
     this.isLoading = true;
     if (this.isLoginMode) {
-
+      authObs = this.authService.logIn(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe(response => {
-        console.log(response);
-        this.isLoading = false;
-      }, errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.isLoading = false;
-      })
+      authObs = this.authService.signUp(email, password);
     }
+    authObs.subscribe(response => {
+      console.log(response);
+      this.isLoading = false;
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+      this.isLoading = false;
+    })
     form.reset();
   }
 }
